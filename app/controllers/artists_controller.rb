8fee86f13bb182a0a7b14eb1c2256730700b6792
Aleksandr Yamschikov <yamshchikov.as73@gmail.com>
2023-08-02 11:09:12 +0200

    Load more button for popular tracks

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,23 @@
 class ArtistsController < ApplicationController
+  MAX_LIMIT = 10
+  DEFAULT_LIMIT = 5
+
   def show
     artist = Artist.find(params[:id])
     albums = selected_albums(artist.albums, params[:album_type]).with_attached_cover.preload(:artist)
-    tracks = artist.tracks.popularity_ordered.limit(5)
+    tracks = artist.tracks.popularity_ordered.limit(limit).offset(offset)
+    tracks_count = artist.tracks.count
+    show_load_more = tracks_count > offset + limit
 
     if turbo_frame_request?
-      render partial: "discography", locals: {artist:, albums:}
+      case turbo_frame_request_id
+      when /popular_tracks/
+        render partial: "popular_tracks", locals: {artist:, tracks:, show_load_more:, limit:, offset:}
+      when /discography/
+        render partial: "discography", locals: {artist:, albums:}
+      end
     else
-      render action: :show, locals: {artist:, albums:, tracks:}
+      render action: :show, locals: {artist:, albums:, tracks:, show_load_more:, offset:, limit:}
     end
   end
 
@@ -19,5 +29,17 @@ class ArtistsController < ApplicationController
     return albums.lp unless Album.kinds.key?(album_type)
 
     albums.where(kind: album_type)
+  end
+
+  def offset
+    params[:offset].to_i || 0
+  end
+
+  def limit
+    if params[:limit].to_i.positive?
+      [MAX_LIMIT, params[:limit].to_i].max
+    else
+      DEFAULT_LIMIT
+    end
   end
 end
